47e0790f499083fd94a78f6fb7d1fdd5ecc28cad
Peter Sankiewicz <peters@vulcan.com>
2022-10-20 20:22:24 -0700

    Create payroll.py

---
+++ payroll.py
@@ -0,0 +1,4 @@
+print(f"Welcome {employeeName}, this system will print your weekly paycheck.\n"
+      f"Our system only accepts real numbers as a valid input.\n"
+      f"The sky is the limit for your pay rate!!!\n"
+      f"We do know that you can't work more hours than there are in a week, so no funny business!!\n")
