aadaabf426a34eae3e8abaf162743915b159bb91
Peter Sankiewicz <peters@vulcan.com>
2022-10-20 20:23:53 -0700

    Update payroll.py

--- payroll.py
+++ payroll.py
@@ -2,3 +2,5 @@ print(f"Welcome {employeeName}, this system will print your weekly paycheck.\n"
       f"Our system only accepts real numbers as a valid input.\n"
       f"The sky is the limit for your pay rate!!!\n"
       f"We do know that you can't work more hours than there are in a week, so no funny business!!\n")
+
+hourlyRate = float(input(f"Enter your desired hourly pay rate: $"))
